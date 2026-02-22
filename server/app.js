e5864f9dc799d8d70832dbcfe1f6afe7ca1e48f3
Kushal Pendekanti <pendekanti.s@northeastern.edu>
2026-02-22 18:21:09 -0500

    fixed for vercel v2

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 
+import { connectDb } from './db.js';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import householdRoutes from './routes/households.js';
@@ -9,6 +10,9 @@ import { authMiddleware } from './middleware/auth.js';
 
 const app = express();
 
+app.use((req, res, next) => {
+  connectDb().then(() => next()).catch(next);
+});
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
