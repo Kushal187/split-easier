87c4cbe35835154f8a8ed00babf12c20f07c7812
Kushal Pendekanti <pendekanti.s@northeastern.edu>
2026-02-22 18:15:21 -0500

    fixed for vercel

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -3,6 +3,9 @@ import app from '../server/app.js';
 
 export default async function handler(req, res) {
   await connectDb();
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? req.url : '/' + req.url);
+  }
   return new Promise((resolve, reject) => {
     app(req, res);
     res.on('finish', resolve);
